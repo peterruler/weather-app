f80890ed883d47ece6fbd3df84d86be88744c5a9
Peter Strössler <7starch@gmail.com>
2025-08-30 12:28:05 +0200

    fresh fix index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ import App from './App';
 
 registerRootComponent(App);
 
+// Ensure environments that import `index.js` directly (e.g., some online runners)
+// can render by providing a default export.
+export default App;
